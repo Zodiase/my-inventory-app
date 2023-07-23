8b57903756cd906d8af33abcfc113a7039a157b4
Xingchen Hong <zodiase@gmail.com>
2023-07-23 04:43:04 -0700

    Fix linter errors.

--- meteor-app/imports/ui/Toolbar.tsx
+++ meteor-app/imports/ui/Toolbar.tsx
@@ -5,22 +5,17 @@ export interface SpacerProps {
     size?: ComponentProps<typeof Box>['basis'];
 }
 
-export const Spacer = ({
-    size,
-    ref,
-    children,
-    ...rootElementProps
-}: SpacerProps & ComponentProps<typeof Box>): ReactElement => {
+export const Spacer = ({ size, children, ...rootElementProps }: SpacerProps & ComponentProps<typeof Box>): ReactElement => {
     if (typeof size === 'undefined') {
-        return <Box ref={ref} basis="0" flex={{ grow: 1, shrink: 1 }} alignSelf="stretch" {...rootElementProps} />;
+        return <Box basis="0" flex={{ grow: 1, shrink: 1 }} alignSelf="stretch" {...rootElementProps} />;
     } else {
-        return <Box ref={ref} basis={size} flex={{ grow: 0, shrink: 1 }} alignSelf="stretch" {...rootElementProps} />;
+        return <Box basis={size} flex={{ grow: 0, shrink: 1 }} alignSelf="stretch" {...rootElementProps} />;
     }
 };
 
-const Toolbar = ({ ref, children, ...rootElementProps }: ComponentProps<typeof Box>): ReactElement => {
+const Toolbar = ({ children, ...rootElementProps }: ComponentProps<typeof Box>): ReactElement => {
     return (
-        <Box ref={ref} align="start" gap="small" direction="row" flex={false} {...rootElementProps}>
+        <Box align="start" gap="small" direction="row" flex={false} {...rootElementProps}>
             {children}
         </Box>
     );
